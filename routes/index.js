3d91ce2218686ce94fa8332edb859c623a8f93e2
LEHUULY-PC\LEHUULY <lehuuly1512313@gmail.com>
2019-05-04 22:17:24 +0700

    +Design Catalog: Sport, Business, Travel, Health - Log-in page

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,25 @@ router.get('/', function (req, res, next) {
 router.get('/blog-post', function (req, res, next) {
 	res.render('blog-post', { title: 'Express' });
 })
+
+router.get('/catalog-sport', function (req, res, next) {
+	res.render('catalog-sport', { title: 'Express' });
+})
+
+router.get('/catalog-business', function (req, res, next) {
+	res.render('catalog-business', { title: 'Express' });
+})
+
+router.get('/catalog-health', function (req, res, next) {
+	res.render('catalog-health', { title: 'Express' });
+})
+
+router.get('/catalog-travel', function (req, res, next) {
+	res.render('catalog-travel', { title: 'Express' });
+})
+
+router.get('/log-in', function (req, res, next) {
+	res.render('log-in', { title: 'Express' });
+})
+
 module.exports = router;

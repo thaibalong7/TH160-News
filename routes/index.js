d0b36026094861e8d80865419bd810bafbec6355
Lý Lê <lehuuly1512313@gmail.com>
2019-06-06 23:10:52 +0700

    +Design WriterEdit.hbs and List.hbs

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,61 @@ router.get('/forgot-password', function (req, res, next) {
 	res.render('forgotpassword', { title: 'Express' });
 })
 
+router.get('/writeredit', function (req, res, next) {
+	res.render('WriterEdit', { title: 'Express' });
+})
+
+router.get('/edit', function (req, res, next) {
+	res.render('WriterEdit', { title: 'Express' });
+})
+
+router.get('/list', function (req, res, next) {
+	res.render('list', { title: 'Express'
+});
+})
+
+router.get('/listdxb', function (req, res, next) {
+	res.render('list', {
+		title: 'Express',
+		news: [{name: 'ĐÁNH GIÁ SAMSUNG GALAXY A70: DÀNH CHO NHỮNG "CƯ DÂN MẠNG THỰC THỤ"', img: 'samsung.png', 
+		author: 'Lê Hữu Lý', type: "Đánh giá",category: "Di Động"}],
+		status: "đã xuất bản"
+	});
+})
+
+router.get('/listcxb', function (req, res, next) {
+	res.render('list', {
+		title: 'Express',
+		news: [{name: 'MICROSOFT CẢNH BÁO VẪN CÒN 1 TRIỆU MÁY TÍNH CHƯA “VÁ” CÁC LỖ HỔNG BẢO MẬT WINDOWS NGHIÊM TRỌNG', img: 'samsung.png', 
+		author: 'Lê Hữu Lý', type: "Đánh giá", category: "Laptop"}],
+		status: "đã được duyệt và chờ xuất bản"
+	});
+})
+
+router.get('/listbtc', function (req, res, next) {
+	res.render('list', { 
+		title: 'Express',
+		news: [{name: 'BỘ CÔNG AN CẢNH BÁO THỦ ĐOẠN MỚI CỦA CÁC HACKER NHẰM CHIẾM ĐOẠT TÀI SẢN CÁC TỔ CHỨC, CÁ NHÂN, DOANH NGHIỆP', img: 'samsung.png', 
+		author: 'Lê Hữu Lý', type: "Đánh giá" ,category: "An Ninh Mạng"},
+		{name: 'BỘ CÔNG AN CẢNH BÁO THỦ ĐOẠN MỚI CỦA CÁC HACKER NHẰM CHIẾM ĐOẠT TÀI SẢN CÁC TỔ CHỨC, CÁ NHÂN, DOANH NGHIỆP', img: 'samsung.png', 
+		author: 'Lê Hữu Lý', type: "Đánh giá" ,category: "An Ninh Mạng"}
+	],
+		edit: 'true',
+		status: "bị từ chối"
+	});
+})
+
+router.get('/listcdd', function (req, res, next) {
+	res.render('list', { 
+		title: 'Express',
+		news: [
+			{name: 'BỎ 10.000 USD MUA CAMERA CÓ ĐỘ PHÂN GIẢI 100 MEGAPIXEL, CÓ ĐÁNG KHÔNG?', img: 'samsung.png', 
+		author: 'Lê Hữu Lý', type: "Đánh giá", category: "Ảnh Số"},
+		{name: 'BỘ CÔNG AN CẢNH BÁO THỦ ĐOẠN MỚI CỦA CÁC HACKER NHẰM CHIẾM ĐOẠT TÀI SẢN CÁC TỔ CHỨC, CÁ NHÂN, DOANH NGHIỆP', img: 'samsung.png', 
+		author: 'Lê Hữu Lý', type: "Đánh giá" ,category: "An Ninh Mạng"}],
+		edit: 'true',
+		status: "chưa được duyệt"
+	});
+})
+
 module.exports = router;

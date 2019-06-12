4a888ed6ca529a37941bee987009b91cb42ea2cc
thaibalong7 <thai.balong.7@gmail.com>
2019-06-12 14:31:13 +0700

    + middleware auth render

---
+++ middleware/auth_render.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const db = require('../models')
+
+const subscribers = db.subscribers;
+const writers = db.writers;
+const editors = db.editors;
+const administrators = db.administrators;
+
+var publicKEY = fs.readFileSync('./middleware/public.key', 'utf8');
+var verifyOptions = {
+    expiresIn: '5d',
+    algorithm: "RS256"
+}
+
+
+var middlewareAuthWriterRender = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        const decode = jwt.verify(token, publicKEY, verifyOptions);
+        if (decode.writer === true) {
+            writers.findOne({
+                where: {
+                    id: decode.id,
+                    email: decode.email
+                }
+            }).then(_user => {
+                if (!_user) {
+                    return res.redirect('/')
+                }
+                else {
+                    req.writerData = _user;
+                    next();
+                }
+            })
+        }
+        else {
+            return res.redirect('/')
+        }
+    } catch (error) {
+        console.log(error)
+        return res.redirect('/');
+    }
+}
+
+module.exports = {
+    middlewareAuthWriterRender
+}
+
